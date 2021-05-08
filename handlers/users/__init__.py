011f93a87eb805a2499b0a6881952bad222e409a
Miron Krivosheev <miron.student2001@bk.ru>
2021-05-08 10:38:19 +0300

    Добавил файл editing_categories в __init__.py в users

--- handlers/users/__init__.py
+++ handlers/users/__init__.py
@@ -1,7 +1,8 @@
-from .start import dp
-from .GlebHandlers import dp
-from .new_category import dp
 
+from .GlebHandlers import dp
+from .editing_category import dp
+from .new_category import dp
+from .start import dp
 from .news_feed_list import dp
 from .help import dp
 from .echo import dp
